fe54419b32b4f02a263c0e827d76e6af60992143
vinicios_auditor <viniciosraymundo@gmail.com>
2023-02-20 16:52:27 -0300

    salvando progresso

--- base.py
+++ base.py
@@ -1,16 +1,26 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
 from PyQt5 import QtCore
 from interface import *
 
 import lista_arquivos
 testando = lista_arquivos.testafuncaonova
 
+
 class Tela(QMainWindow):
     def __init__(self):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
+        ##self.ui.centralwidget.
+        
+
+    #def diretorioA(self):
+    #    pass
+    #    QFileDialog.getOpenFileName(self,"Abrir Pasta")
+        
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
