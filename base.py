70952ad9bb4c2c602cb591cfa348946b2c9d3e50
misternis <viniciosraymundo@gmail.com>
2023-03-05 20:07:06 -0300

    Config TableWidgetB,  ajuste layout centralwidget

--- base.py
+++ base.py
@@ -5,7 +5,7 @@ from PyQt5 import QtCore
 from interface import *
 import lista_arquivos
 import dotenv
-dotenv.load_dotenv(dotenv.find_dotenv())        #Import maluco do Carlos
+dotenv.load_dotenv(dotenv.find_dotenv()) 
 original_dir = os.environ.get("original_dir")   #caminho diretorio A
 backup_dir = os.environ.get("backup_dir")       #caminho diretorio B
 
@@ -21,14 +21,23 @@ class Tela(QMainWindow):
         self.ui.setupUi(self)
         self.ui.botaoDirA.clicked.connect(self.caminhoDirA)        
         self.ui.tableWidgetA.setRowCount(len(diretorioA))
+        self.ui.tableWidgetB.setRowCount(len(diretorioB))
         
-        for i in diretorioA:
+        for i in diretorioA:    #Adiciona items a lista no TableWidgetA
             item_name = QTableWidgetItem(i['name'])
             item_data = QTableWidgetItem(i['date'])
             item_tamanho = QTableWidgetItem(i['size'])
             self.ui.tableWidgetA.setItem(diretorioA.index(i),0,item_name)
             self.ui.tableWidgetA.setItem(diretorioA.index(i),1,item_data)
             self.ui.tableWidgetA.setItem(diretorioA.index(i),2,item_tamanho)
+
+        for i in diretorioB:    #Adiciona items a lista no TableWidgetB
+            item_name = QTableWidgetItem(i['name'])
+            item_data = QTableWidgetItem(i['date'])
+            item_tamanho = QTableWidgetItem(i['size'])
+            self.ui.tableWidgetB.setItem(diretorioB.index(i),0,item_name)
+            self.ui.tableWidgetB.setItem(diretorioB.index(i),1,item_data)
+            self.ui.tableWidgetB.setItem(diretorioB.index(i),2,item_tamanho)
             
 
     @QtCore.pyqtSlot()
