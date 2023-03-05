3ef5d7c4b5eb20b1e140f50e651754ce2598f67e
misternis <viniciosraymundo@gmail.com>
2023-03-04 21:03:45 -0300

    Ajuste para gerar lista e inserir na TableWidgetA

--- base.py
+++ base.py
@@ -10,8 +10,8 @@ original_dir = os.environ.get("original_dir")   #caminho diretorio A
 backup_dir = os.environ.get("backup_dir")       #caminho diretorio B
 
 
-diretorio = lista_arquivos.listarDiretorio(caminho_dir=original_dir)
-diretorio = lista_arquivos.listarDiretorio(caminho_dir=backup_dir)
+diretorioA = lista_arquivos.listarDiretorio(caminho_dir=original_dir)
+diretorioB = lista_arquivos.listarDiretorio(caminho_dir=backup_dir)
 
 
 class Tela(QMainWindow):
@@ -19,25 +19,16 @@ class Tela(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.botaoDirA.clicked.connect(self.caminhoDirA)
-        #self.ui.tableWidgetA.setPlainText(str(lista_arquivos.listarDiretorio(caminho_dir=original_dir)))               # Adiciona a String na lista A
-        #self.ui.tableWidgetA.addItem(str(lista_arquivos.listarDiretorio(caminho_dir=original_dir)))                   # Adiciona a String na lista B    
-        lista = ['Carro', 'Vinicios', 'Bumbum de Nenem', 'igoroi']
-        self.ui.tableWidgetA.setRowCount(len(lista))
-        for i in lista:
-            item_name = QTableWidgetItem(i)
-            item_data = QTableWidgetItem('data')
-            item_tamanho = QTableWidgetItem('tamanho')
-            self.ui.tableWidgetA.setItem(lista.index(i),0,item_name)
-            self.ui.tableWidgetA.setItem(lista.index(i),1,item_data)
-            self.ui.tableWidgetA.setItem(lista.index(i),2,item_tamanho)
-        #result = {'nome':['Vinicios'], 'data': ['05/06/1994'], 'tamanho':['igoroi']}
-        #self.ui.tableWidgetA.clearContents()
-        #self.ui.tableWidgetA.setRowCount(len(result))
-        #for row, text in enumerate(result):
-        #    for column, data in enumerate(text):
-        #        self.ui.tableWidgetA.setItem(row, column, QTableWidgetItem(str(data)))
-
+        self.ui.botaoDirA.clicked.connect(self.caminhoDirA)        
+        self.ui.tableWidgetA.setRowCount(len(diretorioA))
+        
+        for i in diretorioA:
+            item_name = QTableWidgetItem(i['name'])
+            item_data = QTableWidgetItem(i['date'])
+            item_tamanho = QTableWidgetItem(i['size'])
+            self.ui.tableWidgetA.setItem(diretorioA.index(i),0,item_name)
+            self.ui.tableWidgetA.setItem(diretorioA.index(i),1,item_data)
+            self.ui.tableWidgetA.setItem(diretorioA.index(i),2,item_tamanho)
             
 
     @QtCore.pyqtSlot()
--- lista_arquivos.py
+++ lista_arquivos.py
@@ -1,27 +1,13 @@
 import os
-#import dotenv
-#dotenv.load_dotenv(dotenv.find_dotenv())
-#
-#original_dir = os.environ.get("original_dir")
-#backup_dir = os.environ.get("backup_dir")
-
-#def listarDiretorio(caminho_dir):
-#    files = [f for f in os.listdir(caminho_dir) if os.path.isfile(os.path.join(caminho_dir, f))]
-#    files_count = len(files)
-#    print(f'Foram listados {files_count} arquivos:')
-#    for file in files:
-#        print(file)
-#    return
-
-import os
 from datetime import datetime
 
 def listarDiretorio(caminho_dir):
-    files = [f for f in os.listdir(caminho_dir) if os.path.isfile(os.path.join(caminho_dir, f))]
-    texto = ''
+    files = [f for f in os.listdir(caminho_dir) if os.path.isfile(os.path.join(caminho_dir, f))] # Obtém uma lista de arquivos no diretório especificado em "caminho_dir". Em seguida, cria uma nova lista "files" que contém apenas os arquivos (e não diretórios).
+    dados = []
     for file in files:
-        file_path = os.path.join(caminho_dir, file)
-        mod_time = os.path.getmtime(file_path)
-        mod_time_str = datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S')
-        texto += f'{file} ({mod_time_str})\n'
-    return texto
+        file_path = os.path.join(caminho_dir, file)     # Caminho do Arquivo (Path)
+        mod_time = os.path.getmtime(file_path)          # Data do arquivo
+        mod_size = os.path.getsize(file_path) / 1000    # Tamanho do Arquivo em Kbts
+        mod_time_str = datetime.fromtimestamp(mod_time).strftime('%Y-%m-%d %H:%M:%S')      # Data Arquivo e converte em String
+        dados.append({'name': file, 'date': mod_time_str, 'size': ("%.2f" % mod_size + " Kbts").replace('.',',')})      # Adicionar o Dic com as info a lista 'Dados'
+    return dados
